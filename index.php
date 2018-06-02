c2c482e60a3131972bb8f8f859a53818a017fb57
Farid <36966275+Farid-4K@users.noreply.github.com>
2018-06-03 02:56:23 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,28 +1,34 @@
 <?php
-/* Start setting */
 session_start();
 define('ROOT', dirname(__FILE__));
 require_once ROOT . '/app/vendor/autoload.php';
-require_once ROOT . '/app/config/root.php';
+
+use app\core\Router;
+use Dotenv\Dotenv;
+
+/** Load setting */
+$dotenv = new Dotenv(ROOT);
+$dotenv->load();
 
 if (DEBUG == true) {
    ini_set('error_reporting', E_ALL);
    ini_set('display_errors', 1);
    ini_set('display_startup_errors', 1);
    error_reporting(E_ALL);
-}
-
-function debug($str)
-{
-   echo "<pre>";
-   var_dump($str);
-   echo "</pre>";
-   die;
+   if (!function_exists('debug')) {
+      function debug($str)
+      {
+         echo "<pre>";
+         var_dump($str);
+         echo "</pre>";
+         die;
+      }
+   }
 }
 
 spl_autoload_register(
    function ($class) {
-      $path = str_replace('\\', '/', $class . '.php');
+      $path = str_replace('\\', '/', ROOT . '/' . $class . '.php');
       if (file_exists($path)) {
          require_once $path;
       }
@@ -31,8 +37,6 @@ spl_autoload_register(
 /**
  * Begin work
  */
-
-use app\core\Router;
 
 $route = new Router;
 $route->run();
